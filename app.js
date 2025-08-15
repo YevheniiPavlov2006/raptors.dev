8390da9ffbfac30416fc44c0ee390fa8a1376ddb
YevheniiPavlov2006 <jackpavlov9@gmail.com>
2025-08-15 11:25:42 +0300

    merge

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ document.querySelectorAll('[data-footer-animation-items]').forEach(container => 
   const content = container.innerHTML;
   container.innerHTML = content + content; // дублируем один раз
 });
+
+
+
+/*----------------------------------------------pdf-----------------------------------------------*/
+document.querySelectorAll('.pdfViewer').forEach((viewer, i) => {
+    const input = document.querySelectorAll('.pdfInput')[i];
+
+    viewer.addEventListener('click', () => {
+        input.click(); // Открыть диалог выбора файла
+    });
+
+    input.addEventListener('change', function(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const fileURL = URL.createObjectURL(file);
+            viewer.innerHTML = `<iframe src="${fileURL}" width="100%" height="100%" style="border:none;"></iframe>`;
+            viewer.style.border = "none";
+            viewer.style.cursor = "default";
+        }
+    });
+});
